ce75d4a36487b5645ee5a9af4d233e2dcb4dcb2b
JUSTmang11 <peelut48@gmail.com>
2024-08-04 11:55:29 +0700

    hotfixedv1.1

--- libs/dbconn.js
+++ libs/dbconn.js
@@ -5,8 +5,11 @@
 import { initializeApp, cert } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 import fs from 'fs'
+import path from 'path'
+const __dirname = path.resolve();
+// const serviceAccount = JSON.parse(fs.readFileSync('../../Nodelearnong/accound.json', 'utf8'))
 
-const serviceAccount = JSON.parse(fs.readFileSync('../../Nodelearnong/accound.json', 'utf8'))
+const serviceAccount = JSON.parse(fs.readFileSync( __dirname+"/accound.json",'utf8'))
 
 
 initializeApp({
